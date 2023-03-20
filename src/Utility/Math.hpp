9f4ef4733e2decf8e2cdffad53e7fdce2ab9ec64
MunirZ <munirzohbi@gmail.com>
2023-03-20 13:53:43 +0100

    Funktion für Kosinusähnlichkeit wird berechnet

--- src/Utility/Math.hpp
+++ src/Utility/Math.hpp
@@ -1,21 +1,10 @@
+#include <vector>
+#include <cmath>
+#include <numeric>
+#include <stdexcept>
+
 namespace Utility::Math {
 
-struct Point
-{
-    double x;
-    double y;
-};
+double cosine_similarity(const std::vector<double>& first_vector, const std::vector<double>& second_vector);
 
-struct Vector 
-{
-    Point a;
-    Point b;
-};
-
-double calc_cosine_similarity(Vector first_vector, Vector second_vector)
-{
-    double result = 0;
-    // TODO
-    return result;
-}
 }
