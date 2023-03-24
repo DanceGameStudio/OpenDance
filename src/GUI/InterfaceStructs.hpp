fd96498fcf305d734663979e8cd5bba9ba9e0146
MunirZ <munirzohbi@gmail.com>
2023-03-25 00:57:30 +0100

    Interface und Structs Definiert

---
+++ src/GUI/InterfaceStructs.hpp
@@ -0,0 +1,26 @@
+#pragma once 
+
+#include "ScoreBoard.hpp"
+
+namespace Interface {
+
+struct Settings {
+    int gain = 0;
+};
+
+struct Graphics {
+    // cv::Mat video_image;
+    // cv::Mat camera_image;
+};
+
+struct Player {
+    std::string name;
+    std::string score;
+    std::string timespamp;
+};
+
+struct ScoreBoard {
+    std::vector<Player> player_list;
+};
+
+}
