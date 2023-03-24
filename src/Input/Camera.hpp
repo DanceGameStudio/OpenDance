587956f9a3ad682b2924efb90c53510b1817a1ae
MunirZ <munirzohbi@gmail.com>
2023-03-25 00:32:03 +0100

    Definition von Input, Video und Camera Klasse

--- src/Input/Camera.hpp
+++ src/Input/Camera.hpp
@@ -4,9 +4,14 @@
 
 namespace Input {
 
-class Camera : Input 
+class Camera : public Input 
 {
-	
+    // cv::Mat& read() override;
+
+    void resize(const int width, const int height) override;
+
+    void change_color_space(const ColorSpace colorSpace) override;
+
 };
 
 }
