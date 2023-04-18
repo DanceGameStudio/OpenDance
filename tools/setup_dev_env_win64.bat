18e77daf98fbe7eebc89280d71f7573090cd476f
LeonTheo02 <leontheo293@gmail.com>
2023-04-18 09:55:16 +0200

    Fixed #9

--- tools/setup_dev_env_win64.bat
+++ tools/setup_dev_env_win64.bat
@@ -7,3 +7,8 @@ echo Unpacking OpenPose...
 7zip\7za.exe x -y -o..\3rdparty\openpose\bin\ ..\3rdparty\openpose\bin\cublas64_11.7z
 7zip\7za.exe x -y -o..\3rdparty\openpose\bin\ ..\3rdparty\openpose\bin\cublasLt64_11.7z
 7zip\7za.exe x -y -o..\3rdparty\openpose\bin\ ..\3rdparty\openpose\bin\opencv_world420d.7z
+
+echo Downloading OpenPose Models...
+pushd ..\3rdparty\openpose\models\
+getBaseModels.bat
+getCOCO_and_MPII_optional.bat
