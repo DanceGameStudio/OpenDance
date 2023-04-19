56cf9bae66719870d11b45dec3e9caf5c281a7c9
derTunnel <102300274+derTunnel@users.noreply.github.com>
2023-04-19 08:21:31 +0200

    - mit der Posenerfassung angefangen

--- src/PoseEstimation/PoseDetector.cpp
+++ src/PoseEstimation/PoseDetector.cpp
@@ -1,8 +1,57 @@
 #include <chrono>
-
 #include "PoseDetector.hpp"
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
 namespace PoseEstimation {
+
+void PoseDetector::start()
+{
+    std::cout << "TEST PostDetector";
+    camera_start();
+}
+
+int PoseDetector::camera_start()
+{
+    cv::VideoCapture cap;
+    cap.open(0);
+
+    // Check if camera opened successfully
+    if (!cap.isOpened()) {
+        std::cout << "Error opening video stream or file" << std::endl;
+        return -1;
+    }
+
+    while (true) {
+
+        cv::Mat frame;
+        // Capture frame-by-frame
+        cap >> frame;
+
+        // If the frame is empty, break immediately
+        if (frame.empty())
+            break;
+
+        // Display the resulting frame
+        cv::imshow("Frame", frame);
+
+        // Press  ESC on keyboard to exit
+        char c = (char)cv::waitKey(25);
+        if (c == 27)
+            break;
+    }
+
+    // When everything done, release the video capture object
+    cap.release();
+
+    // Closes all the frames
+    cv::destroyAllWindows();
+
+    return 0;
+}
+
+
+
 
 //Pose PoseDetector::get_pose(cv::Mat& image)
 //{
--- src/PoseEstimation/PoseDetector.hpp
+++ src/PoseEstimation/PoseDetector.hpp
@@ -9,9 +9,14 @@ namespace PoseEstimation {
 class PoseDetector 
 {
     //Pose get_pose(cv::Mat& image);
-    
+public:
+    void start();
+    int camera_start();
+
+
 private:
     //std::vector<Keypoint> detect_keypoints(cv::Mat& image);
+
 
     std::vector<Pose> poses_;
 };
