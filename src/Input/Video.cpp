95be0aa08abd57d0f0d54e5ec6c13e7917deb3d0
MunirZ <munirzohbi@gmail.com>
2023-04-19 10:21:05 +0200

    Linker Fehler mit OpenCV gefixed

--- src/Input/Video.cpp
+++ src/Input/Video.cpp
@@ -2,3 +2,21 @@
 
 #include "Video.hpp"
 
+namespace Input {
+
+cv::Mat& Video::read()
+{
+    cv::Mat image = cv::Mat::zeros(cv::Size(width_, height_), CV_8UC4);
+    return image;
+}
+
+void Video::resize(const int width, const int height)
+{
+    
+}
+
+void Video::change_color_space(const ColorSpace colorSpace)
+{
+}
+
+}
