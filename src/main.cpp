74ab108484fe9d5d7ccc9af283451c5dec8931cf
Ben Laptop <Benn20002@gmail.com>
2023-04-27 11:30:46 +0200

    ---

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,26 @@
 #include "GameLogic.hpp"
 #include "MainWindow.cpp"
 #include "../Interfaces/GuiInterface.hpp"
+#include <thread>
 
 int main([[maybe_used]] int argc, [[maybe_used]] char** argv)
 {
     auto intf = std::make_shared<Interface::GuiInterface>();
 
-    std::unique_ptr<GameLogic::GameLogic> game = std::make_unique<GameLogic::GameLogic>(intf);
-    game->loop();
+    std::thread thrd_game([&]() {
+        std::unique_ptr<GameLogic::GameLogic> game = std::make_unique<GameLogic::GameLogic>(intf);
+        while (true) {
+            game->loop();
+        }
+    });
+    
+    std::thread thrd_gui([&]() {
+        GUI gui(intf);
+        gui.run(argc, argv);
+    });
 
-    GUI gui(intf);
-    gui.run(argc, argv);
+    thrd_game.join();
+    thrd_gui.join();
 
     return 0;
 }
