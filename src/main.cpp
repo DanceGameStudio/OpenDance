5a4d38d5a673164ac75a6e8137415eb8e03f22a0
Ben Laptop <Benn20002@gmail.com>
2023-04-27 13:22:20 +0200

    stop game logic thread when gui is stopped

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,30 @@
-#include <opencv2/core.hpp>
+#include "../Interfaces/GuiInterface.hpp"
 #include "GameLogic.hpp"
 #include "MainWindow.cpp"
-#include "../Interfaces/GuiInterface.hpp"
+#include <opencv2/core.hpp>
 #include <thread>
 
 int main([[maybe_used]] int argc, [[maybe_used]] char** argv)
 {
     auto intf = std::make_shared<Interface::GuiInterface>();
 
+    bool stopGame = false;
+
     std::thread thrd_game([&]() {
         std::unique_ptr<GameLogic::GameLogic> game = std::make_unique<GameLogic::GameLogic>(intf);
-        while (true) {
+        while (!stopGame) {
             game->loop();
         }
     });
-    
+
     std::thread thrd_gui([&]() {
         GUI gui(intf);
         gui.run(argc, argv);
+        stopGame = true;
     });
 
-    thrd_game.join();
     thrd_gui.join();
+    thrd_game.join();
 
     return 0;
 }
