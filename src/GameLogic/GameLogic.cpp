85bbcf8fa4207f6a79f0330574a2b2a36c6e8b79
Ben-PC <Benn20002@gmail.com>
2023-05-03 12:33:55 +0200

    changed Interface Structure

--- src/GameLogic/GameLogic.cpp
+++ src/GameLogic/GameLogic.cpp
@@ -5,15 +5,15 @@ namespace GameLogic {
 void GameLogic::loop()
 {
     cv::Mat camera_image = graphics_->camera_->read();
-    //cv::Mat video_image = graphics_->video_->read();
+    cv::Mat video_image = graphics_->video_->read();
     //
     //PoseEstimation::Pose camera_pose = pose_analyser_->detector_->get_pose(&camera_pose);
     //PoseEstimation::Pose video_pose = pose_analyser_->detector_->get_pose(&video_image);
     // 
     //pose_analyser_->compare_poses(camera_pose, video_pose);
-    Interface::Graphics graphics = Interface::Graphics();
-    graphics.camera_image = camera_image;
-    interface_->set_graphics(graphics);
+    auto graphics = interface_->get_graphics();
+    graphics->camera_image = camera_image;
+    graphics->video_image = video_image;
 }
 
 void GameLogic::load_configuration()
