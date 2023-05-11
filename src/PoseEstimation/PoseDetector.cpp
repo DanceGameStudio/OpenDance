54819298e2a7ad60083a7838f6d4a9fcf007c3ff
Munir <m.zohbi@marinom.de>
2023-05-11 10:13:15 +0200

    Nur CPU-DLLs werden geladen

--- src/PoseEstimation/PoseDetector.cpp
+++ src/PoseEstimation/PoseDetector.cpp
@@ -24,21 +24,21 @@ Pose PoseDetector::get_pose(const cv::Mat& image)
 std::vector<Keypoint> PoseDetector::detect_keypoints(const cv::Mat& image)
 {
     std::vector<Keypoint> detected_keypoints;
-    if (wrapper_configured_) {
-        auto datumProcessed = op_wrapper_.emplaceAndPop(OP_CV2OPCONSTMAT(image));
-        if (datumProcessed != nullptr && !datumProcessed->empty()) {
-            op::Array<float> keypoints = datumProcessed->at(0)->poseKeypoints;
-            int coco_keypoint_size = 18;
-            // Berwertung nur für Person 0 und 2D
-            try {
+    try {
+        if (wrapper_configured_) {
+            auto datumProcessed = op_wrapper_.emplaceAndPop(OP_CV2OPCONSTMAT(image));
+            if (datumProcessed != nullptr && !datumProcessed->empty()) {
+                op::Array<float> keypoints = datumProcessed->at(0)->poseKeypoints;
+                int coco_keypoint_size = 18;
+                // Berwertung nur für Person 0 und 2D
                 for (int i = 0; i < coco_keypoint_size; i++) {
                     Keypoint point = { keypoints[0, i, 0], keypoints[0, i, 1], 0 };
                     detected_keypoints.push_back(point);
                 }
-            } catch (...) {
-                std::cout << "Keypoint Array broken!" << std::endl;
             }
         }
+    } catch (...) {
+        std::cout << "Keypoint Array broken!" << std::endl;
     }
     return detected_keypoints;
 }
